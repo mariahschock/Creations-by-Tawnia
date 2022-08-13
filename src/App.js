eb85450f5c9bcfb99f7df9df4afadddcb97ad3c6
Mariah <mariahschock@gmail.com>
2022-08-12 17:49:48 -0700

    Created  homepage component and set up router

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import HomePage from './HomePage';
 import './App.css';
 
 function App() {
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          <nav>
+            <div className="links">
+              <Link to="/">Home</Link>
+            </div>
+          </nav>
+          <div className="contact-btn">
+            <button>Contact</button>
+          </div>
+        </header>
+       
+      
+
+        <Switch>
+          <Route exact path="/">
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/HomePage.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function HomePage() {
+  return (
+    <div className="home-page">
+      <h1>Welcome to Creations by Tawnia</h1>
+    </div>
+  );
+}
